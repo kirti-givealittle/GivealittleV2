dea8180157ddc3c9e2e2e4ab583bebbf55035055
visal-givealittle <visal.kathriarachchi@givealittle.co.nz>
2026-01-11 14:22:59 +1300

    Added DBMigrator app

---
+++ GivealittleV2/GivealittleV2.DbMigrator/Program.cs
@@ -0,0 +1,31 @@
+﻿using DbUp;
+using System.Reflection;
+
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    Console.WriteLine("DB_CONNECTION is not set.");
+    return;
+}
+
+var upgrader =
+    DeployChanges.To
+        .SqlDatabase(connectionString)
+        .WithScriptsFromFileSystem("../../../Migrations")
+        .LogToConsole()
+        .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+    Environment.Exit(-1);
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Database upgrade successful");
+Console.ResetColor();
